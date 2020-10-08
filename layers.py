cc6f0dea752d9268674b3bd400ad58fe95972637
Isaac Rehg <isaacrehg@gmail.com>
2020-10-08 14:49:11 -0400

    add files for low-rank mixture layers

---
+++ layers.py
@@ -0,0 +1,314 @@
+import torch.nn as nn
+import torch
+
+
+class Null(nn.Module):
+    def __init__(self):
+        super(Null, self).__init__()
+        self.null = None
+
+    def forward(self, x):
+        if self.null is None:
+            self.null = torch.zeros_like(x).to(x.device)
+            self.null.requires_grad = False
+        return self.null
+
+
+class Pass(nn.Module):
+    def __init__(self, detach=False):
+        super(Pass, self).__init__()
+        self.detach = detach
+
+    def forward(self, x):
+        if self.detach:
+            x = x.detach()
+        return x
+
+
+class HashLinear(nn.Linear):
+    def __init__(self, in_features, out_features, n_keys, optim='gd', detach=False, cache_attn=False, **kwargs):
+        super(HashLinear, self).__init__(in_features, out_features, **kwargs)
+        self.detach = detach
+        self.do_cache = cache_attn
+        self.cached_attn = None
+        self.n_keys = n_keys
+
+        self.key_op = nn.Linear(in_features, n_keys, bias=False)
+        self.softmax = nn.Softmax(dim=1)
+        self.val_op = nn.Linear(n_keys, out_features, bias=False)
+
+    @property
+    def hash_keys(self):
+        return self.key_op.weight.data
+
+    @property
+    def hash_vals(self):
+        return self.val_op.weight.data
+
+    def cache_attn(self, cache=True):
+        self.do_cache = cache
+
+    def forward(self, x):
+        if self.detach:
+            x = x.detach()
+        sims = self.key_op(x)  # [batch_size x n_keys]
+        attn = self.softmax(sims)
+        if self.cache_attn:
+            self.cached_attn = attn.data.cpu()
+        out = self.val_op(attn)  # [batch_size x out_features]
+        return out
+
+
+class HashConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, n_keys,
+                 optim='gd', detach=False, cache_attn=False, padding=(1, 1), stride=(1, 1), t=1.0, hard=False):
+        super(HashConv, self).__init__()
+        self.hard = hard
+        self.sm_temperature = t
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.detach = detach
+        self.do_cache = cache_attn
+        self.cached_attn = None
+        self.avg_entropy = None
+        self.n_keys = n_keys
+
+        self.key_op = nn.Conv2d(in_channels, n_keys, kernel_size, padding=padding, stride=stride, bias=False)
+        self.softmax = nn.Softmax(dim=1)
+        self.val_op = nn.Conv2d(n_keys, out_channels, (1, 1), bias=False)
+
+    @property
+    def hash_keys(self):
+        return self.key_op.weight.data
+
+    @property
+    def hash_vals(self):
+        return self.val_op.weight.data
+
+    def cache_attn(self, cache=True):
+        self.do_cache = cache
+
+    def set_temperature(self, t):
+        self.sm_temperature = t
+
+    def decay_temperature(self, decay):
+        self.sm_temperature *= (1 - decay)
+
+    def compute_attn_entropy(self, x):
+        average_entropy = -(x * torch.log(x)).sum(dim=1).mean()
+        self.avg_entropy = average_entropy
+
+    def forward(self, x):
+        if self.detach:
+            x = x.detach()
+        sims = self.key_op(x)  # [batch_size x n_keys x height x width]
+
+        # if hard is true and we are evaluating, compute hard max operation instead of softmax
+        if (not self.training) and self.hard:
+            attn = torch.zeros_like(sims).to(sims.device)
+            attn[sims == sims.max(dim=1)] = 1.0
+        else:
+            attn = self.softmax(sims / self.sm_temperature)
+
+        self.compute_attn_entropy(attn)
+
+        if self.do_cache:
+            self.cached_attn = attn.data.cpu()
+
+        out = self.val_op(attn)  # [batch_size x out_channels x height x width]
+
+        return out
+
+
+class LRMLinearV1(nn.Linear):
+    def __init__(self, in_features, out_features, n_blocks, block_size, optim='gd', detach=False,
+                 cache_attn=False, **kwargs):
+        super(LRMLinearV1, self).__init__(in_features, out_features, **kwargs)
+        self.detach = detach
+        self.do_cache = cache_attn
+        self.cached_attn = None
+        self.n_blocks = n_blocks
+        self.block_size = block_size
+
+        self.key_op = nn.Linear(in_features, n_blocks, bias=False)
+        self.softmax = nn.Softmax(dim=1)
+        self.V_op = nn.Linear(in_features, n_blocks * block_size, bias=False)
+        self.U_op = nn.Linear(n_blocks * block_size, out_features, bias=False)
+
+    @property
+    def hash_keys(self):
+        return self.key_op.weight.data
+
+    @property
+    def V(self):
+        return self.V_op.weight.data
+
+    @property
+    def U(self):
+        return self.U_op.weight.data
+
+    def cache_attn(self, cache=True):
+        self.do_cache = cache
+
+    def forward(self, x):
+        if self.detach:
+            x = x.detach()
+        sims = self.key_op(x)           # [batch_size x n_blocks]
+        attn = self.softmax(sims)
+        if self.cache_attn:
+            self.cached_attn = attn.data.cpu()
+        out = self.V_op(x)              # [batch_size x n_blocks * block_size]
+        out = out * attn[:, :, None].repeat(1, 1, self.block_size).reshape(-1, self.n_blocks * self.block_size)  # naive
+        out = self.U_op(out)            # [batch_size x out_features]
+        return out
+
+
+class LRMConvV1(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, n_blocks, block_size,
+                 optim='gd', detach=False, cache_attn=False, padding=(1, 1), stride=(1, 1), t=1.0, hard=False):
+        super(LRMConvV1, self).__init__()
+        self.hard = hard
+        self.sm_temperature = t
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.detach = detach
+        self.do_cache = cache_attn
+        self.cached_attn = None
+        self.avg_entropy = None
+        self.n_blocks = n_blocks
+        self.block_size = block_size
+
+        self.key_op = nn.Conv2d(in_channels, n_blocks, kernel_size, padding=padding, stride=stride, bias=False)
+        self.softmax = nn.Softmax(dim=1)
+        self.V_op = nn.Conv2d(in_channels, n_blocks * block_size, kernel_size,
+                              padding=padding, stride=stride, bias=False)
+        self.U_op = nn.Conv2d(n_blocks * block_size, out_channels, (1, 1), bias=False)
+
+    @property
+    def hash_keys(self):
+        return self.key_op.weight.data
+
+    @property
+    def hash_vals(self):
+        return self.val_op.weight.data
+
+    def cache_attn(self, cache=True):
+        self.do_cache = cache
+
+    def set_temperature(self, t):
+        self.sm_temperature = t
+
+    def decay_temperature(self, decay):
+        self.sm_temperature *= (1 - decay)
+
+    def compute_attn_entropy(self, x):
+        average_entropy = -(x * torch.log(x)).sum(dim=1).mean()
+        self.avg_entropy = average_entropy
+
+    def forward(self, x):
+        h, w = x.shape[2:]
+        if self.detach:
+            x = x.detach()
+        sims = self.key_op(x)  # [batch_size x n_blocks x height x width]
+
+        # if hard is true and we are evaluating, compute hard max operation instead of softmax
+        if (not self.training) and self.hard:
+            attn = torch.zeros_like(sims).to(sims.device)
+            attn[sims == sims.max(dim=1)] = 1.0
+        else:
+            attn = self.softmax(sims / self.sm_temperature)
+
+        self.compute_attn_entropy(attn)
+
+        if self.do_cache:
+            self.cached_attn = attn.data.cpu()
+
+        attn = attn[:, :, None].repeat(1, 1, self.block_size, 1, 1).reshape(-1, self.n_blocks * self.block_size, h, w)  # naive
+                                    # [batch_size x n_blocks * block_size x height x width]
+
+        out = self.V_op(x) * attn   # [batch_size x n_blocks * block_size x height x width]
+        out = self.U_op(out)        # [batch_size x out_channels x height x width]
+
+        return out
+
+
+# Modified BasicBlock used by ResNet18 - https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+class InjectBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None,
+                 inject_conv="hash", remove_bn2=True, detach=True, n_keys=256, block_size=64, cache_attn=False, temperature=1.0,
+                 cut_residual=False):
+        super(InjectBlock, self).__init__()
+
+        ###################################
+        remove_bn2 = remove_bn2 and (inject_conv is not None)
+        self.detach = detach
+        self.cut_residual = cut_residual
+        ###################################
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        ###################################
+        self.inject_conv = inject_conv
+        if inject_conv == "pass":
+            self.conv2 = Pass(detach=detach)
+        elif inject_conv == "hash":
+            self.conv2 = HashConv(planes, planes, (3, 3), n_keys,
+                                  detach=detach, cache_attn=cache_attn, t=temperature)
+        elif inject_conv == "block":
+            self.conv2 = BlockConv(planes, planes, (3, 3), n_keys, block_size,
+                                   detach=detach, cache_attn=cache_attn, t=temperature)
+        else:
+            self.conv2 = conv3x3(planes, planes)
+
+        self.bn2 = norm_layer(planes)
+
+        if remove_bn2:
+            self.bn2 = Pass()
+        ###################################
+
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        ###################################
+        # cut gradient to residual if only training downstream layers
+        if self.detach:
+            identity = identity.detach()
+        ###################################
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        ###################################
+        # cut the residual connection if specified
+        if not self.cut_residual:
+            out += identity
+
+        if self.inject_conv == "null":
+            out = identity
+        ###################################
+
+        out = self.relu(out)
+
+        return out
---
+++ model.py
@@ -0,0 +1,162 @@
+import torch.nn as nn
+from layers import *
+from resnet import *
+
+
+def _block_size(in_dim, out_dim, n_blocks):
+    """Returns block_size setting that ensures same memory cost as
+        vanilla layer with corresponding in_dim and out_dim"""
+    return int(in_dim * out_dim / (in_dim + out_dim) / n_blocks)
+
+
+def _block_size_conv(in_ch, out_ch, filter_h, filter_w, n_blocks):
+    return _block_size(in_ch * filter_h * filter_w, out_ch, n_blocks)
+
+
+def _block_size_conv3x3(in_ch, out_ch, n_blocks):
+    return _block_size_conv(in_ch, out_ch, 3, 3, n_blocks)
+
+
+def lrm3x3(in_planes, out_planes, n_blocks, block_size, stride=1, cache_attn=False):
+    """3x3 low-rank mixture convolution with padding"""
+    return LRMConvV1(in_planes, out_planes, 3, n_blocks, block_size,
+                     stride=stride, padding=1, cache_attn=cache_attn)
+
+
+def lrm1x1(in_planes, out_planes, n_blocks, block_size, stride=1, cache_attn=False):
+    """1x1 low-rank mixture convolution with padding"""
+    return LRMConvV1(in_planes, out_planes, 1, n_blocks, block_size,
+                     stride=stride, padding=0, cache_attn=cache_attn)
+
+
+class LRMBlockV1(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None,
+                 n_blocks=4, cache_attn=False):
+        super(LRMBlockV1, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = lrm3x3(inplanes, planes, n_blocks, _block_size_conv3x3(inplanes, planes, n_blocks),
+                            stride=stride, cache_attn=cache_attn)
+        self.bn1 = norm_layer(planes)
+        self.conv2 = lrm3x3(planes, planes, n_blocks, _block_size_conv3x3(planes, planes, n_blocks),
+                            stride=stride, cache_attn=cache_attn)
+        self.bn2 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        if downsample is not None:
+            self.downsample = nn.Sequential(
+                lrm1x1(downsample[0].in_channels, downsample[0].out_channels, n_blocks,
+                       _block_size_conv3x3(downsample[0].in_channels, downsample[0].out_channels,
+                                        n_blocks),
+                       stride=downsample[0].stride, cache_attn=cache_attn),
+                norm_layer(downsample[0].out_channels)
+            )
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class HashBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
+                 base_width=64, dilation=1, norm_layer=None,
+                 detach=False, n_keys=256, cache_attn=False, temperature=1.0,
+                 cut_residual=False):
+        super(HashBlock, self).__init__()
+
+        ###################################
+        self.detach = detach
+        self.cut_residual = cut_residual
+        ###################################
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        ###################################
+        self.hashconv = HashConv(planes, planes, (3, 3), n_keys,
+                                 detach=detach, cache_attn=cache_attn, t=temperature)
+        ###################################
+
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        ###################################
+        # cut gradient to residual if only training downstream layers
+        if self.detach:
+            identity = identity.detach()
+        ###################################
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.hashconv(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        ###################################
+        # cut the residual connection if specified
+        if not self.cut_residual:
+            out += identity
+        ###################################
+
+        out = self.relu(out)
+
+        return out
+
+
+def lrm_resnet18(**kwargs):
+    r"""ResNet-34 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    return build_resnet('resnet18', LRMBlockV1, [2, 2, 2, 2], False, True,
+                        **kwargs)
+
+
+# test models
+if __name__ == '__main__':
+    lrm_resnet18()
+    resnet18()
