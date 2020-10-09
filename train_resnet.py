7ca61c46201344b7f4874117db7ccf74f8a29e5d
Isaac Rehg <isaacrehg@gmail.com>
2020-10-09 00:36:06 -0400

    rename train and create training script for resnet

---
+++ train_resnet.py
@@ -0,0 +1,12 @@
+from experiment_utils.train_models import get_dataloaders, save_model, test, train
+from experiment_utils.argument_parsing import DataArgs, TrainingArgs, parse_args
+from model import resnet18
+
+
+if __name__ == '__main__':
+    data_args, train_args = parse_args(DataArgs, TrainingArgs)
+    train_loader, val_loader, test_loader = get_dataloaders(data_args)
+    model = resnet18()
+    model.cuda()
+    train(train_args, model, train_loader, val_loader, device=0)
+
