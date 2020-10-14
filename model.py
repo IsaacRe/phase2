ed1df08e77002c1281cd2236b09b955557876782
Isaac Rehg <isaacrehg@gmail.com>
2020-10-14 00:21:59 -0400

    refactor model.py

--- model.py
+++ model.py
@@ -29,6 +29,7 @@ def lrm1x1(in_planes, out_planes, n_blocks, block_size, stride=1, cache_attn=Fal
                      stride=stride, padding=0, cache_attn=cache_attn)
 
 
+# Modified from BasicBlock - https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 class LRMBlockV1(nn.Module):
     expansion = 1
 
@@ -48,16 +49,6 @@ class LRMBlockV1(nn.Module):
 
         block_size_conv1 = _block_size_conv3x3(inplanes, planes, n_blocks, alpha=block_size_alpha)
         block_size_conv = _block_size_conv3x3(planes, planes, n_blocks, alpha=block_size_alpha)
-
-        if downsample is not None:
-            ds_conv, ds_bn = downsample
-            ds_in_ch, ds_out_ch, ds_stride = ds_conv.in_channels, ds_conv.out_channels, ds_conv.stride
-            block_size_downsample = _block_size(ds_in_ch, ds_out_ch, n_blocks, alpha=block_size_alpha)
-            downsample = nn.Sequential(
-                lrm1x1(ds_in_ch, ds_out_ch, n_blocks, block_size_downsample,
-                       stride=ds_stride, cache_attn=cache_attn),
-                norm_layer(ds_out_ch)
-            )
 
         self.conv1 = lrm3x3(inplanes, planes, n_blocks, block_size_conv1, stride=stride, cache_attn=cache_attn)
         self.bn1 = norm_layer(planes)
@@ -87,6 +78,7 @@ class LRMBlockV1(nn.Module):
         return out
 
 
+# Modified from BasicBlock - https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
 class HashBlock(nn.Module):
     expansion = 1
 
@@ -149,12 +141,63 @@ class HashBlock(nn.Module):
         return out
 
 
-class LRMResNetV1(ResNet):
+# Modified from ResNet - https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
+class LRMResNetV1(nn.Module):
 
-    def __init__(self, block, layers, n_blocks=1, block_size_alpha=1, **kwargs):
+    def __init__(self, block, layers, num_classes=100, zero_init_residual=False,
+                 groups=1, width_per_group=64, replace_stride_with_dilation=None, norm_layer=None,
+                 n_blocks=1, block_size_alpha=1):
+        super(LRMResNetV1, self).__init__()
         self.n_blocks = n_blocks
         self.block_size_alpha = block_size_alpha
-        super(LRMResNetV1, self).__init__(block, layers, **kwargs)
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = LRMConvV1(3, self.inplanes, (7, 7), n_blocks,
+                               _block_size_conv(3, self.inplanes, 7, 7, n_blocks, alpha=block_size_alpha),
+                               stride=2, padding=3)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
 
     def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
         norm_layer = self._norm_layer
@@ -164,8 +207,11 @@ class LRMResNetV1(ResNet):
             self.dilation *= stride
             stride = 1
         if stride != 1 or self.inplanes != planes * block.expansion:
+            block_size_downsample = _block_size(self.inplanes, planes * block.expansion, self.n_blocks,
+                                                alpha=self.block_size_alpha)
             downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes * block.expansion, stride),
+                lrm1x1(self.inplanes, planes * block.expansion, self.n_blocks, block_size_downsample,
+                       stride=stride),
                 norm_layer(planes * block.expansion),
             )
 
@@ -181,6 +227,27 @@ class LRMResNetV1(ResNet):
                                 n_blocks=self.n_blocks, block_size_alpha=self.block_size_alpha))
 
         return nn.Sequential(*layers)
+
+    def _forward_impl(self, x):
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x):
+        return self._forward_impl(x)
 
 
 def _lrm_resnet(Version, block, layers, num_classes=100, seed=1, disable_bn_stats=False, **kwargs):
