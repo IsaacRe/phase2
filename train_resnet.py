1544de63981d6efcb719f318a610775185b44a11
Isaac Rehg <isaacrehg@gmail.com>
2021-01-05 14:11:28 -0500

    add option to load pretrained model when training vanilla resnet

--- train_resnet.py
+++ train_resnet.py
@@ -1,11 +1,21 @@
+import torch
 from experiment_utils.train_models import get_dataloaders, save_model, test, train
 from experiment_utils.argument_parsing import *
 from model import resnet18
 
 
+class LoadModelArgs(ArgumentClass):
+    ARGS = {
+        'load_model_path':
+            Argument('--load-model-path', type=str, default=None, help='path to model file to load at init')
+    }
+
+
 if __name__ == '__main__':
-    data_args, train_args = parse_args(DataArgs, TrainingArgs)
+    model_args, data_args, train_args = parse_args(LoadModelArgs, DataArgs, TrainingArgs)
     train_loader, val_loader, test_loader = get_dataloaders(data_args)
     model = resnet18(num_classes=data_args.num_classes, seed=data_args.seed)
+    if model_args.load_model_path:
+        model.load_state_dict(torch.load(model_args.load_model_path))
     model.cuda()
     train(train_args, model, train_loader, val_loader, device=0)
